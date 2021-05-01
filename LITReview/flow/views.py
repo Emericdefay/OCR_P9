d912c34b41f86d572d797caa953199e6f3da78ec
emericdefay <emericdefay@gmail.com>
2021-05-01 18:41:23 +0200

    rework flow filter and pimp flox template

--- LITReview/flow/views.py
+++ LITReview/flow/views.py
@@ -14,19 +14,31 @@ def flow(request):
         request ([type]): [description]
 
     Returns:
-        [type]: [description]
+        [render]: [description]
+            request: 
+            template:
+            context:
+                data:
+                range:
     """
     tickets = Ticket.objects.all()
     reviews = Review.objects.all()
-    
+    users = UserFollows.objects.all()
+    # Tickets
+    # 1. Get all tickets from user
+    # 2. Get all tickets from followed_user-s
     intersection_tickets = (
-        tickets.filter(user=request.user) |
-        tickets.filter(user__id__in=UserFollows.objects.filter(user=request.user).values_list("followed_user_id")))
-
+    tickets.filter(user=request.user) |
+    tickets.filter(user__id__in=users.filter(user=request.user).values_list("followed_user_id"))
+    )
+    # Reviews
+    # 1. Get all reviews from user
+    # 2. Get all reviews from followed_user-s
+    # 3. Get reviews from un-followed_user-s that review the user's ticket
     intersection_reviews = (
     reviews.filter(user=request.user) |
-    reviews.filter(user__id__in=UserFollows.objects.filter(user=request.user).values_list("followed_user_id")) |
-    reviews.filter(ticket__id__in=Review.objects.filter(id__in=Ticket.objects.all().values_list("id")).values_list("ticket_id"))
+    reviews.filter(user__id__in=users.filter(user=request.user).values_list("followed_user_id")) |
+    reviews.filter(ticket__id__in=tickets.filter(user=request.user).values_list("id"))
     )
 
     flow_posts = list(sorted(chain(intersection_tickets, intersection_reviews),
